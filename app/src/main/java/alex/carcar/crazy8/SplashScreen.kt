3f1432af097e79c05be531fef9148aec3157c704
Alex Carcar <44008979+acarcar-trellis@users.noreply.github.com>
2021-01-18 16:01:12 -0800

    - added play button and detected click

--- app/src/main/java/alex/carcar/crazy8/SplashScreen.kt
+++ app/src/main/java/alex/carcar/crazy8/SplashScreen.kt
@@ -10,19 +10,22 @@ import android.view.View
 class SplashScreen(context: Context?) : View(context) {
     private val paint: Paint = Paint()
     private val titleG: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.splash_graphic)
+    private val playBtnUp: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.btn_up)
+    private val playBtnDn: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.btn_down)
+    private var playBtnPressed: Boolean = false
     private var cx: Int = 0
     private var cy: Int = 0
     private val radius: Float = 50f
     private val tag: String = getContext().javaClass.name
 
     private var scrW: Int = 0
-    private var scrH : Int = 0
+    private var scrH: Int = 0
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         this.scrW = w
         this.scrH = h
         this.cx = ((w - this.radius) / 2).toInt()
-        this.cy = h/6
+        this.cy = h / 6
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -30,14 +33,43 @@ class SplashScreen(context: Context?) : View(context) {
         cx += 50
         cy += 25
         canvas.drawCircle(cx.toFloat(), cy.toFloat(), radius, paint)
-        val titleGLeftPos = (scrW - titleG.width)/2
+        val titleGLeftPos = (scrW - titleG.width) / 2
         canvas.drawBitmap(titleG, titleGLeftPos.toFloat(), 100f, null)
+        val playBtnLeftPos = (scrW - playBtnUp.width) / 2
+        val playBtnTopPos = scrH / 2
+        if (playBtnPressed) {
+            canvas.drawBitmap(playBtnDn, playBtnLeftPos.toFloat(), playBtnTopPos.toFloat(), null)
+        } else {
+            canvas.drawBitmap(playBtnUp, playBtnLeftPos.toFloat(), playBtnTopPos.toFloat(), null)
+        }
     }
 
     override fun onTouchEvent(evt: MotionEvent): Boolean {
+        val x = evt.x
+        val y = evt.y
+
         when (evt.action) {
-            MotionEvent.ACTION_DOWN -> Log.d(tag, "Down")
-            MotionEvent.ACTION_UP -> Log.d(tag, "Up")
+            MotionEvent.ACTION_DOWN -> {
+                cx = x.toInt()
+                cy = y.toInt()
+                Log.d(tag, "Down")
+                val btnLeft = (scrW - playBtnUp.width) / 2
+                val btnRight = btnLeft + playBtnUp.width
+                val btnTop = scrH * 0.5
+                val btnBottom = btnTop + playBtnUp.height
+                val withinBounds = x > btnLeft && x < btnRight && y > btnTop && y < btnBottom
+                if (withinBounds) {
+                    playBtnPressed = true
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.d(tag, "Up")
+                if (playBtnPressed) {
+                    // Launch main game screen
+                    Log.d(tag, "Launch main game screen")
+                }
+                playBtnPressed = false
+            }
             MotionEvent.ACTION_MOVE -> {
                 Log.d(tag, "Move")
                 cx = evt.x.toInt()
